a1cb5a02d17344bb2bba8a2bdebc5854b7bc3536
Natanaël RALAIVOAVY <ralaivoavy.natanael@gmail.com>
2024-07-08 22:29:06 +0200

    update : register cli and register pass

--- frontend/src/pages/auth/register/RegisterPass.jsx
+++ frontend/src/pages/auth/register/RegisterPass.jsx
@@ -1,46 +1,39 @@
-import { Button, TextInput } from "../../../styles/components";
+import { Button, Icon, TextInput } from "../../../styles/components";
 import { SERVERLINK } from "../../../constants";
 import { useAuth } from "../../../context/AuthProvider";
+import { useForm } from "../../../context/FormProvider";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const RegisterPass = () => {
-  const {inputs, setInputs, setAuth, getInformation} = useAuth();
-  
   const {
-    firstname,
-    lastname,
-    usercin,
-    companynumber,
-    phone,
-    adress,
-    email,
-    bio,
-    profileimage,
-    accountid,
-    password,
-    confirmPassword,
-  } = inputs;
+    inputs,
+    setInputs,
+    setErrorData,
+    errorData,
+    setAuth,
+    getInformation,
+  } = useAuth();
 
-  console.log(inputs);
+  const { handleInputChange, handleError } = useForm();
+  const [fieldError, setFieldError] = useState(false);
+  const navigate = useNavigate();
 
-  const handleChange = (e) => {
-    setInputs({ ...inputs, [e.target.name]: e.target.value });
-  };
-
-  const onSubmitForm = async (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
     try {
       const body = {
         firstname,
         lastname,
-        usercin,
-        companynumber,
+        userCin,
+        companyNumber,
         phone,
         adress,
         email,
         bio,
-        profileimage,
-        accountid,
+        profileImage,
+        accountId,
         password,
         confirmPassword,
       };
@@ -65,68 +58,54 @@ const RegisterPass = () => {
     }
   };
 
+  useEffect(() => {
+    // Check if all of the current fielfs are valid
+    const { password, confirmPassword } = errorData;
+    setFieldError(password || confirmPassword);
+  }, [inputs, errorData]);
+
   return (
-    <section className="w-fullscreen">
-      <div className="flex mt-5 ml-3">
-        <span className="w-[5px] h-[100px] bg-maintr-100 mr-2"></span>
-        <div>
-          <p className="text-subtitle-2 boxShadow text-gray-100">
-            Connectez-vous pour commencer a utiliser
-          </p>
-          <br />
-          <p className="text-subtitle-1">
-            Media <span className="text-maintr-100">Trans</span>
-          </p>
+    <section className="w-fullscreen bg-gray-80 auth-section space-y-[128px] absolute top-[128px] left-1/2 -translate-x-1/2">
+      <div className="flex flex-col items-center justify-center gap-4">
+        <div className="w-full flex items-center justify-center">
+          <Icon icon="bi bi-arrow-left" onClick={() => navigate(-1)} />
+        </div>
+        <div className="h1 text-subtitle-1 w-full text-center">
+          Créer un <span className="text-primary-100">mot de passe</span>
         </div>
       </div>
       <form
-        className="mt-[10px] flex justify-center items-center"
-        onSubmit={onSubmitForm}
+        className="flex flex-col  border border-black-20 bg-white-100 items-start justify-center gap-[32px] w-fit p-4 rounded-xl"
+        onSubmit={handleSubmit}
       >
-        <div>
-          <div className="mb-[50px] flex items-center justify-center mr-5 text-subtitle-3">
-            <span className="bi bi-arrow-left bg-primary-60 py-2 px-4 rounded-[50%]"></span>
-          </div>
-          <div className="mb-5">
-            <label htmlFor="password" className="text-small-1 text-gray-100">
-              Mot de passe
-            </label>
+        <div className="flex flex-col items-start w-full justify-center gap-6">
+          <h3 className="text-subtitle-3 text-black-100">Sécurité</h3>
+          <div className="flex flex-col items-start justify-center gap-6">
             <TextInput
-              onChange={(e) => handleChange(e)}
-              className="w-[275px]"
-              type="password"
-              placeholder=""
               name="password"
-              id="password"
-            />
-          </div>
-          <div className="mb-5">
-            <label
-              htmlFor="confirmPassword"
-              className="text-small-1 text-gray-100"
-            >
-              Confirmez
-            </label>
-            <TextInput
-              onChange={(e) => handleChange(e)}
-              className="w-[275px]"
+              title="Adresse"
               type="password"
-              placeholder=""
+              placeholder="Entrer votre mot de passe"
+              onError={handleError(setErrorData)}
+              onChange={(e) => handleInputChange(setInputs, e)}
+              value={inputs.password}
+            />
+            <TextInput
               name="confirmPassword"
-              id="password"
+              title="Confirmer"
+              type="password"
+              isValid={inputs.confirmPassword === inputs.password}
+              placeholder="Confirmer votre mot de passe"
+              onError={handleError(setErrorData)}
+              onChange={(e) => handleInputChange(setInputs, e)}
+              value={inputs.confirmPassword}
             />
           </div>
-          <Button className="w-[275px] mt-5">Creer le compte</Button>
         </div>
+        <Button disabled={fieldError} block>
+          Suivant
+        </Button>
       </form>
-      <div className="mt-[10px]">
-        <div>
-          <a href="\mdp" className="text-small-2 text-gray-100">
-            Déjà inscris ?
-          </a>
-        </div>
-        <Button className="w-[275px] mt-5">Se connecter</Button>
-      </div>
     </section>
   );
 };
--- frontend/src/pages/labo/Labo.jsx
+++ frontend/src/pages/labo/Labo.jsx
@@ -1,34 +1,77 @@
-import {  useNavigate } from "react-router-dom";
-import { Button, TextArea, TextInput } from "../../styles/components.js"
+import { useNavigate } from "react-router-dom";
+import { Button, Icon, TextArea, TextInput } from "../../styles/components.js";
 import { useAuth } from "../../context/AuthProvider.jsx";
 import { useForm } from "../../context/FormProvider.jsx";
 import { useEffect, useState } from "react";
 
-
 const Labo = () => {
-    const { inputs, setInputs, setErrorData, errorData } = useAuth();
-    const { handleInputChange, handleError } =
-      useForm();
-    const [fieldError, setFieldError] = useState(false)
-;
+  const {
+    inputs,
+    setInputs,
+    setErrorData,
+    errorData,
+    setAuth,
+    getInformation,
+  } = useAuth();
+
+  const { handleInputChange, handleError } = useForm();
+  const [fieldError, setFieldError] = useState(false);
   const navigate = useNavigate();
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    navigate("/registerPass");
+
+    try {
+      const body = {
+        firstname,
+        lastname,
+        userCin,
+        companyNumber,
+        phone,
+        adress,
+        email,
+        bio,
+        profileImage,
+        accountId,
+        password,
+        confirmPassword,
+      };
+
+      const response = await fetch(SERVERLINK + "/api/auth/signup", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body),
+      });
+
+      const parseRes = await response.json();
+
+      if (!parseRes.error) {
+        localStorage.setItem("token", parseRes.token);
+        setAuth(true);
+        getInformation();
+      }
+
+      console.log(parseRes.error);
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   useEffect(() => {
     // Check if all of the current fielfs are valid
-    const {firstname, companyNumber, email, phone, adress, bio} = errorData;
-    setFieldError(firstname || companyNumber || email || phone || adress || bio);
+    const { password, confirmPassword } = errorData;
+    setFieldError(password || confirmPassword);
   }, [inputs, errorData]);
-
 
   return (
     <section className="w-fullscreen bg-gray-80 auth-section space-y-[128px] absolute top-[128px] left-1/2 -translate-x-1/2">
-      <div className="h1 text-subtitle-1 w-full text-center">
-        Entrer vos <span className="text-primary-100">identifications</span>
+      <div className="flex flex-col items-center justify-center gap-4">
+        <div className="w-full flex items-center justify-center">
+          <Icon icon="bi bi-arrow-left" onClick={() => navigate(-1)} />
+        </div>
+        <div className="h1 text-subtitle-1 w-full text-center">
+          Créer un <span className="text-primary-100">mot de passe</span>
+        </div>
       </div>
       <form
         className="flex flex-col  border border-black-20 bg-white-100 items-start justify-center gap-[32px] w-fit p-4 rounded-xl"
@@ -36,63 +79,26 @@ const Labo = () => {
       >
         <div className="flex flex-col items-start w-full justify-center gap-6">
           <h3 className="text-subtitle-3 text-black-100">Identification</h3>
-          <div className="flex flex-row items-start justify-center gap-6">
-            <div className="flex flex-col items-center justify-center gap-6">
-              <TextInput
-                name="companyNumber"
-                title="Numéro de l'entreprise"
-                placeholder=""
-                onError={handleError(setErrorData)}
-                onChange={(e) => handleInputChange(setInputs, e)}
-                value={inputs.comanyNumber}
-              />
-              <TextInput
-                name="firstname"
-                title="Nom de l'entreprise"
-                placeholder="Entrer le nom de votre enterprise"
-                onError={handleError(setErrorData)}
-                onChange={(e) => handleInputChange(setInputs, e)}
-                value={inputs.firstname}
-              />
-              <TextInput
-                name="email"
-                title="Email"
-                type="email"
-                placeholder="Entrer votre email"
-                onError={handleError(setErrorData)}
-                onChange={(e) => handleInputChange(setInputs, e)}
-                value={inputs.email}
-              />
-              <TextInput
-                name="phone"
-                title="Telephone"
-                type="texte"
-                placeholder="Entrer votre numéro de téléphone"
-                onError={handleError(setErrorData)}
-                onChange={(e) => handleInputChange(setInputs, e)}
-                value={inputs.phone}
-              />
-            </div>
-            <div className="flex flex-col items-center justify-center gap-6">
-              <TextInput
-                name="adress"
-                title="Adresse"
-                type="texte"
-                placeholder="Entrer votre adresse"
-                onError={handleError(setErrorData)}
-                onChange={(e) => handleInputChange(setInputs, e)}
-                value={inputs.adress}
-              />
-
-              <TextArea
-                name="bio"
-                title="Description"
-                placeholder="Entrer une description"
-                onError={handleError(setErrorData)}
-                onChange={(e) => handleInputChange(setInputs, e)}
-                value={inputs.bio}
-              />
-            </div>
+          <div className="flex flex-col items-start justify-center gap-6">
+            <TextInput
+              name="password"
+              title="Adresse"
+              type="password"
+              placeholder="Entrer votre mot de passe"
+              onError={handleError(setErrorData)}
+              onChange={(e) => handleInputChange(setInputs, e)}
+              value={inputs.password}
+            />
+            <TextInput
+              name="confirmPassword"
+              title="Confirmer"
+              type="password"
+              isValid={inputs.confirmPassword === inputs.password}
+              placeholder="Confirmer votre mot de passe"
+              onError={handleError(setErrorData)}
+              onChange={(e) => handleInputChange(setInputs, e)}
+              value={inputs.confirmPassword}
+            />
           </div>
         </div>
         <Button disabled={fieldError} block>
@@ -102,6 +108,5 @@ const Labo = () => {
     </section>
   );
 };
-
 
 export default Labo;
