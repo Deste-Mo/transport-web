24312cd65d1532236cc88926518b10766670bb0e
Deste-mo <modestep20.aps1a@gmail.com>
2024-07-17 13:15:36 +0300

    config pg

--- frontend/connexion.js
+++ frontend/connexion.js
@@ -1,7 +1,10 @@
+// Importer dotenv pour charger les variables d'environnement à partir de .env
 require('dotenv').config();
 
+// Importer le module pg pour la connexion PostgreSQL
 const { Pool } = require('pg');
 
+// Configurer la connexion PostgreSQL en utilisant les variables d'environnement chargées
 const pool = new Pool({
     user: process.env.PGUSER,
     host: process.env.PGHOST,
@@ -10,6 +13,7 @@ const pool = new Pool({
     port: process.env.PGPORT,
 });
 
+// Tester la connexion en exécutant une requête simple
 pool.query('SELECT NOW()', (err, res) => {
     if (err) {
         console.error('Erreur lors de la connexion à PostgreSQL', err);
@@ -17,9 +21,5 @@ pool.query('SELECT NOW()', (err, res) => {
         console.log('Connexion à PostgreSQL établie avec succès.');
         console.log('La date courante est :', res.rows[0].now);
     }
-    pool.end(); 
+    pool.end(); // Terminer la connexion lorsque vous avez fini
 });
-
-
-backend/src/db/connexion.js
-backend/src/routes/notificationsRoutes.js
